6501b435516fb8ea5ba88c6058aafbe8fdee9d25
Jason L <Jas0n17L1n@gmail.com>
2025-11-05 12:50:23 -0500

    Feat: Added card learning feature under Practice tab

---
+++ read_right_app/lib/widgets/flip_card.dart
@@ -0,0 +1,171 @@
+// FILE: lib/widgets/flip_card.dart
+// PURPOSE: Animated card that flips between word and sentences with tap gesture
+// TOOLS: Flutter animation controller and transform widgets
+
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class FlipCard extends StatefulWidget {
+  final String word;
+  final String sentence1;
+  final String sentence2;
+
+  const FlipCard({
+    super.key,
+    required this.word,
+    required this.sentence1,
+    required this.sentence2,
+  });
+
+  @override
+  State<FlipCard> createState() => _FlipCardState();
+}
+
+class _FlipCardState extends State<FlipCard>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _flipAnimation;
+  bool _isShowingFront = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 600),
+      vsync: this,
+    );
+    _flipAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _flip() {
+    if (_isShowingFront) {
+      _animationController.forward();
+    } else {
+      _animationController.reverse();
+    }
+    _isShowingFront = !_isShowingFront;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _flip,
+      child: AnimatedBuilder(
+        animation: _flipAnimation,
+        builder: (context, child) {
+          final isShowingFront = _flipAnimation.value < 0.5;
+          return Transform(
+            alignment: Alignment.center,
+            transform: Matrix4.identity()
+              ..setEntry(3, 2, 0.001)
+              ..rotateY(_flipAnimation.value * math.pi),
+            child: Card(
+              elevation: 8,
+              child: Container(
+                width: double.infinity,
+                height: 300,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: isShowingFront
+                      ? Colors.blue.shade50
+                      : Colors.green.shade50,
+                ),
+                child: isShowingFront ? _buildFrontSide() : _buildBackSide(),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildFrontSide() {
+    return Container(
+      padding: const EdgeInsets.all(32),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              widget.word,
+              style: const TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              'Tap to see sentences',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBackSide() {
+    return Transform(
+      alignment: Alignment.center,
+      transform: Matrix4.identity()..rotateY(math.pi),
+      child: Container(
+        padding: const EdgeInsets.all(32),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(
+                Icons.format_quote,
+                size: 32,
+                color: Colors.green,
+              ),
+              const SizedBox(height: 16),
+              Text(
+                widget.sentence1,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              Text(
+                widget.sentence2,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Tap to go back to word',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
